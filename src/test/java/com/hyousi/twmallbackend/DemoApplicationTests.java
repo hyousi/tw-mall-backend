3f8c9d04527d9abe8b3881f12019f54b5219fa39
Ze Dang <4649tz@gmail.com>
2020-08-14 13:43:35 +0800

    [zedang] feat: initial project.

---
+++ src/test/java/com/hyousi/twmallbackend/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyousi.twmallbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
